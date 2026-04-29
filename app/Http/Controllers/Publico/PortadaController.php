d33a757dca065ed6baca4bfb27f2192b9bf8d6d7
Adrian_C <3341802@alu.murciaeduca.es>
2026-04-29 08:43:06 +0200

    layouts, vistas y controladores principales

---
+++ app/Http/Controllers/Publico/PortadaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Publico;
+
+use App\Http\Controllers\Controller;
+use App\Models\Modulo;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PortadaController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): View
+    {
+        $modulos = Modulo::with([
+                'cicloFormativo.familiaProfesional',
+                'ecosistemasLaborales' => fn($q) => $q->where('activo', true),
+            ])
+            ->whereHas('ecosistemasLaborales', fn($q) => $q->where('activo', true))
+            ->take(6)->get();
+
+        return view('publico.portada', compact('modulos'));
+    }
+}
